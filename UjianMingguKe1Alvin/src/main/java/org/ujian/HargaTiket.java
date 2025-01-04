7572af0dd7d9fec962e58455baf44eb89f5270a5
alvinchandrahadiyanto <chandrahadiyantoalvin@gmail.com>
2025-01-04 23:51:40 +0700

    Menambahkan try catch ke HargaTiket.java

--- UjianMingguKe1Alvin/src/main/java/org/ujian/HargaTiket.java
+++ UjianMingguKe1Alvin/src/main/java/org/ujian/HargaTiket.java
@@ -15,24 +15,30 @@ public class HargaTiket {
         System.out.println("5.Jumat");
         System.out.println("6.Sabtu");
         System.out.println("7.Minggu");
-        System.out.print("Masukan Nilai Hari Ini (1-7): ");
-        int day = input.nextInt();
-        if (day < 1 || day > 7) {
-            System.out.println("Masukan Nilai Hari yang benar");
-            System.exit(0);
-        } else if (day > 5) {
-            hargaTiket = 45000;
+
+        try {
+            System.out.print("Masukan Nilai Hari Ini (1-7): ");
+            int day = input.nextInt();
+            if (day < 1 || day > 7) {
+                System.out.println("Masukan Nilai Hari yang benar");
+                System.exit(0);
+            } else if (day > 5) {
+                hargaTiket = 45000;
+            }
+            System.out.print("Masukan Jumlah Tiket Yang Ingin dibeli: ");
+            int jumlahTiket = input.nextInt();
+            if (jumlahTiket < 1) {
+                System.out.println("Jumlah Tiket tidak boleh kurang dari 1");
+            } else if (jumlahTiket < jumlahMinimalTiketDapatDiskon) {
+                System.out.println("Jumlah Tiket: "+jumlahTiket);
+                System.out.println("Total Harga: "+hargaTiket*jumlahTiket);
+            }else {
+                System.out.println("Jumlah Tiket: "+jumlahTiket);
+                System.out.println("Total Harga: "+hargaTiket*jumlahTiket*(1-diskon));
+            }
         }
-        System.out.print("Masukan Jumlah Tiket Yang Ingin dibeli: ");
-        int jumlahTiket = input.nextInt();
-        if (jumlahTiket < 1) {
-            System.out.println("Jumlah Tiket tidak boleh kurang dari 1");
-        } else if (jumlahTiket < jumlahMinimalTiketDapatDiskon) {
-            System.out.println("Jumlah Tiket: "+jumlahTiket);
-            System.out.println("Total Harga: "+hargaTiket*jumlahTiket);
-        }else {
-            System.out.println("Jumlah Tiket: "+jumlahTiket);
-            System.out.println("Total Harga: "+hargaTiket*jumlahTiket*(1-diskon));
+        catch(Exception e) {
+            System.out.println(e.toString());
         }
     }
 }
