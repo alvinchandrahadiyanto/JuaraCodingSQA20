2b750bb3ff171e3814b98ad17aa2db30984acfd6
alvinchandrahadiyanto <chandrahadiyantoalvin@gmail.com>
2025-01-19 07:46:47 +0700

    Menambahkan materi dan ujian minggu ke 2

---
+++ UjianMingguKe2Alvin/src/test/java/org/BelajarTesNG/CalculatorTest.java
@@ -0,0 +1,39 @@
+package org.BelajarTesNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void add(){
+        int actualResult = Calculator.add(10,20);
+        int expectedResult = 30;
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void subtract(){
+        int actualResult = Calculator.subtract(10,20);
+        int expectedResult = -10;
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void multiplication(){
+        int actualResult = Calculator.subtract(10,20);
+        int expectedResult = 200;
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void Modulo(){
+        int actualResult = Calculator.subtract(10,20);
+        int expectedResult = 10;
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
